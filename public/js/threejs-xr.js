dbc876bce81341fe0623c4d00d81d12c1e64bfd8
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-08 00:11:08 -0600

    controller factory works

--- public/js/threejs-xr.js
+++ public/js/threejs-xr.js
@@ -1,16 +1,23 @@
 "use strict";
 
 import * as THREE from "./libraries/threejs/build/three.module.js";
+import { XRControllerModelFactory } from './libraries/threejs/examples/jsm/webxr/XRControllerModelFactory.js';
 
 class ThreeXr {
 
-	constructor(_renderer, _camera, _scene) {
+	constructor(_renderer, _camera, _scene, _useControllerFactory) {
 		this.renderer = _renderer;
 		this.camera = _camera;
 		this.scene = _scene;
 
+		this.useControllerFactory = _useControllerFactory;
+
+		this.raycaster;
+
 		this.controller0;
 		this.controller1;
+		this.controllerGrip0;
+		this.controllerGrip1;
 		this.pivot0;
 		this.pivot1;
 		this.xrReady = false;
@@ -18,11 +25,6 @@ class ThreeXr {
 
 	onTriggerStart() {
 		this.userData.trigger = true;
-		if (this === this.controller0) {
-			this.userData.target = new THREE.Vector3().setFromMatrixPosition(this.pivot0.matrixWorld);
-		} else {
-			this.userData.target = new THREE.Vector3().setFromMatrixPosition(this.pivot1.matrixWorld);
-		}
 	}
 
 	onTriggerEnd() {
@@ -55,20 +57,51 @@ class ThreeXr {
 	}
 
 	makeControllerMeshes() {
-		const geometry = new THREE.CylinderGeometry(0.01, 0.02, 0.08, 5);
-		geometry.rotateX(-Math.PI / 2);
-		const material = new THREE.MeshStandardMaterial({ flatShading: true });
-		const mesh = new THREE.Mesh(geometry, material);
-
 		const pivot = new THREE.Mesh(new THREE.IcosahedronGeometry(0.01, 3));
 		pivot.name = 'pivot';
-		pivot.position.z = - 0.05;
-		mesh.add(pivot);
+		pivot.position.z = -0.05;
 
-		this.controller0.add(mesh.clone());
+		if (this.useControllerFactory) {
+			const controllerModelFactory = new XRControllerModelFactory();
+			
+			this.controllerGrip0 = this.renderer.xr.getControllerGrip(0);
+			this.controllerGrip0.add(controllerModelFactory.createControllerModel(this.controllerGrip0));
+			this.scene.add(this.controllerGrip0);
+
+			this.controllerGrip1 = this.renderer.xr.getControllerGrip(1);
+			this.controllerGrip1.add(controllerModelFactory.createControllerModel(this.controllerGrip1));
+			this.scene.add(this.controllerGrip1);
+
+			this.controller0.add(pivot.clone());
+			this.controller1.add(pivot.clone());
+		} else {
+			const geometry = new THREE.CylinderGeometry(0.01, 0.02, 0.08, 5);
+			geometry.rotateX(-Math.PI / 2);
+			const material = new THREE.MeshStandardMaterial({ flatShading: true });
+			const mesh = new THREE.Mesh(geometry, material);
+			mesh.add(pivot);
+
+			this.controller0.add(mesh.clone());
+			this.controller1.add(mesh.clone());
+		}
+
 		this.pivot0 = this.controller0.getObjectByName("pivot");
-		this.controller1.add(mesh.clone());
 		this.pivot1 = this.controller1.getObjectByName("pivot");
+
+		this.xrReady = true;
+	}
+
+	setupRaycaster() {
+		const geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -1)]);
+
+		const line = new THREE.Line(geometry);
+		line.name = "line";
+		line.scale.z = 5;
+
+		this.controller0.add(line.clone());
+		this.controller1.add(line.clone());
+
+		this.raycaster = new THREE.Raycaster();
 	}
 
 	setupXr() {
@@ -87,8 +120,6 @@ class ThreeXr {
 		this.scene.add(this.controller1);
 
 		this.makeControllerMeshes();
-
-		this.xrReady = true;
 	}
 
 	updateXr() {
