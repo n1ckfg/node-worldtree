d65ebd1c9555e00b9d6a0e9eb0b4886de020cb3a
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-15 16:19:49 -0400

    Update worldscale.js

--- public/js/worldscale.js
+++ public/js/worldscale.js
@@ -4,19 +4,21 @@
 
 class WorldScale {
 
-    WorldScale() {
-	    this.ctl0;
-	    this.ctl1;
+    WorldScale(_ctl0, _ctl1) {
+	    this.ctl0 = _ctl0;
+	    this.ctl1 = _ctl1;
 	    this.target; // Transform
-
 	    this.armed = false;
-	    this.SevenMode { BOTH, MAIN, ALT, NONE };
-	    this.sevenMode = SevenMode.NONE;
+	    
+	    this.SevenMode = { BOTH: "both", MAIN: "main", ALT: "alt", NONE: "none" };
+	    this.sevenMode = this.SevenMode.NONE;
+	    
 	    this.initialHandPosition1; // first controller
 	    this.initialHandPosition2; // second controller
 	    this.initialObjectRotation; // this.target rotation, Quaternion
 	    this.initialObjectScale; // this.target scale
 	    this.initialObjectDirection; // direction of this.target to midpoint of both controllers
+	    
 	    this.origParent = this.target.parent;
     }
 
@@ -27,36 +29,36 @@ class WorldScale {
         }
 
         if (this.ctl0.gripped && this.ctl1.gripped) {
-            sevenMode = SevenMode.BOTH;
+            this.sevenMode = this.SevenMode.BOTH;
         } else if (this.ctl0.gripped && !this.ctl1.gripped) {
-            sevenMode = SevenMode.MAIN;
+            this.sevenMode = this.SevenMode.MAIN;
         } else if (!this.ctl0.gripped && this.ctl1.gripped) {
-            sevenMode = SevenMode.ALT;
+            this.sevenMode = this.SevenMode.ALT;
         } else if (!this.ctl0.gripped && !this.ctl1.gripped) {
-            sevenMode = SevenMode.NONE;
-            this.target.SetParent(origParent);
+            this.sevenMode = this.SevenMode.NONE;
+            this.target.SetParent(this.origParent);
             this.armed = false;
             return;
         }
 
         if (this.armed) {
-            switch (sevenMode) {
+            switch (this.sevenMode) {
                 case SevenMode.BOTH:
-                    attachTargetBoth();
+                    this.attachTargetBoth();
                     break;
                 case SevenMode.MAIN:
-                    attachTargetOne(ref this.ctl0);
+                    this.attachTargetOne(this.ctl0);
                     break;
                 case SevenMode.ALT:
-                    attachTargetOne(ref this.ctl1);
+                    this.attachTargetOne(this.ctl1);
                     break;
             }
             this.armed = false;
         }
 
-        switch (sevenMode) {
-            case SevenMode.BOTH:
-                updateTargetBoth();
+        switch (this.sevenMode) {
+            case this.SevenMode.BOTH:
+                this.updateTargetBoth();
                 break;
         }
 
@@ -67,33 +69,33 @@ class WorldScale {
         this.initialHandPosition2 = this.ctl1.transform.position;
         this.initialObjectRotation = this.target.transform.rotation;
         this.initialObjectScale = this.target.transform.localScale;
-        this.initialObjectDirection = this.target.transform.position - (initialHandPosition1 + initialHandPosition2) * 0.5; 
+        this.initialObjectDirection = this.target.transform.position - (this.initialHandPosition1 + this.initialHandPosition2) * 0.5; 
     }
 
     updateTargetBoth() {
         const currentHandPosition1 = this.ctl0.transform.position; // current first hand position
         const currentHandPosition2 = this.ctl1.transform.position; // current second hand position
 
-        const handDir1 = (initialHandPosition1 - initialHandPosition2).normalized; // direction vector of initial first and second hand position
+        const handDir1 = (this.initialHandPosition1 - this.initialHandPosition2).normalized; // direction vector of initial first and second hand position
         const handDir2 = (currentHandPosition1 - currentHandPosition2).normalized; // direction vector of current first and second hand position 
 
         const handRot = Quaternion.FromToRotation(handDir1, handDir2); // calculate rotation based on those two direction vectors
         
         const currentGrabDistance = Vector3.Distance(currentHandPosition1, currentHandPosition2);
-        const initialGrabDistance = Vector3.Distance(initialHandPosition1, initialHandPosition2);
+        const initialGrabDistance = Vector3.Distance(this.initialHandPosition1, this.initialHandPosition2);
         const p = (currentGrabDistance / initialGrabDistance); // percentage based on the distance of the initial positions and the new positions
 
-        const newScale = new Vector3(p * initialObjectScale.x, p * initialObjectScale.y, p * initialObjectScale.z); // calculate new object scale with p
+        const newScale = new Vector3(p * this.initialObjectScale.x, p * this.initialObjectScale.y, p * this.initialObjectScale.z); // calculate new object scale with p
 
-        this.target.transform.rotation = handRot * initialObjectRotation; // add rotation
+        this.target.transform.rotation = handRot * this.initialObjectRotation; // add rotation
         this.target.transform.localScale = newScale; // set new scale
         
         // set the position of the object to the center of both hands based on the original object direction relative to the new scale and rotation
-        this.target.transform.position = (0.5 * (currentHandPosition1 + currentHandPosition2)) + (handRot * (initialObjectDirection * p));
+        this.target.transform.position = (0.5 * (currentHandPosition1 + currentHandPosition2)) + (handRot * (this.initialObjectDirection * p));
     }
 
-    attachTargetOne(ref SteamVR_NewController ctl) {
-        this.target.SetParent(ctl.transform);
+    attachTargetOne(_ctl) {
+        this.target.SetParent(_ctl.transform);
     }
 
 }
